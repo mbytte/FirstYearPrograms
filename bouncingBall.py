ab35e65d8372eb48146953d545f1beebb240e9b9
mbytte <meganloud52@gmail.com>
2024-04-16 19:32:15 +0200

    Added a bouncing ball program

---
+++ bouncingBall.py
@@ -0,0 +1,28 @@
+import stddraw
+
+RADIUS = .05
+DT = 20.0
+
+def main():
+    stddraw.setXscale(-1.0, 1.0)
+    stddraw.setYscale(-1.0, 1.0)
+
+    rx = .480
+    ry = .860
+    vx = .015
+    vy = .023
+
+    while True:
+        # Update ball position and draw it there.
+        if abs(rx + vx) + RADIUS > 1.0: vx = -vx
+        if abs(ry + vy) + RADIUS > 1.0: vy = -vy
+        rx = rx + vx
+        ry = ry + vy
+
+        stddraw.clear(stddraw.GRAY)
+        stddraw.filledCircle(rx, ry, RADIUS)
+        stddraw.show(3)
+        
+#running the program
+if __name__ == '__main__':
+    main()
